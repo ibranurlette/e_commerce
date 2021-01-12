32abe8e89eac58783098c9cec59e27fe676d0de6
Ibra Nurlette <sjsdev02@gmail.com>
2021-01-12 17:04:02 +0700

    screen cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
 
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -28,12 +28,16 @@ const App = () => {
     fetchCart();
   }, []);
 
-  console.log(cart.total_items);
-
   return (
     <div>
       <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+
+      {Object.entries(cart).length === 0 ? (
+        <h1>LOADINGS....</h1>
+      ) : (
+        <Cart cart={cart} />
+      )}
     </div>
   );
 };
