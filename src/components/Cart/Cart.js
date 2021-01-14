605568ed8138ac5f63bcaaa0771ace591d82d26e
Ibra Nurlette <sjsdev02@gmail.com>
2021-01-14 09:48:07 +0700

    add screencheckout with step

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -57,6 +57,8 @@ const Cart = ({
               Empty Cart
             </Button>
             <Button
+              component={Link}
+              to="/checkout"
               className={classes.checkoutButton}
               size="large"
               type="button"
---
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const steps = ["Shipping address", "Payment detail"];
+
+const Checkout = () => {
+  const classes = useStyles();
+  const [activeStep, SetActiveStep] = useState(0);
+  return (
+    <>
+      <div className={classes.toolbar}>
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography variant="h4" align="center">
+              Checkout
+            </Typography>
+            <Stepper activeStep={activeStep} className={classes.stepper}>
+              {steps.map((step) => (
+                <Step key={step}>
+                  <StepLabel>{step}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Paper>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
